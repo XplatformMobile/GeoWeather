74294ad8c577927aced146250dfa92934640184f
msoe <msoe@msoemac06siMac.nebula.msoe.edu>
2017-05-03 14:54:54 -0500

    Improved styling for the locations list page to lower the header and table down 20 pixels.  This creates room at the top for Carrier, time and battery icons

--- app/alloy.js
+++ app/alloy.js
@@ -7,7 +7,7 @@ if (OS_IOS || OS_ANDROID) {
 	Alloy.Globals.Map = Ti.Map = require('ti.map');
 	Alloy.Collections.location = Alloy.createCollection('location');
 	Alloy.Globals.top = 0;
-	Alloy.Globals.tableTop = '50dp';
+	Alloy.Globals.tableTop = '70dp';	// offset for locations list screen (=) button
 }
 Alloy.Globals.winTop = (OS_IOS && parseInt(Ti.Platform.version, 10) >= 7) ? 20 : 0;
 Ti.UI.backgroundColor = "#fff";
